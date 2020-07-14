0341b2a71a3118149e01dd9028fd55b63c434ed4
SHINOBU ISHIDA <SHINOBUI@jp.ibm.com>
2020-07-14 10:36:35 +0900

    update config.yml(add running powershell)

---
+++ javascript/tools/spLibUpload.ps1
@@ -0,0 +1,53 @@
+<#
+概要：
+指定したSharePointサイトのライブラリに、指定したディレクトリ配下のファイルをアップロードする
+実行形式：
+./spLibUpload.ps1 -url [サイトのURL] -library [アップロード先ライブラリ名] -username [ユーザー名] -password [パスワード] -path [対象のファイルのあるディレクトリパス]
+#>
+#
+# SharePoint Online Automation – O365 – Upload files to document library using PowerShell CSOM
+# https://global-sharepoint.com/powershell/upload-files-to-sharepoint-online-document-library-using-powershell-csom/
+
+
+# 引数取得(サイトURL、ユーザー名、パスワード、登録するファイルのあるフォルダ)
+Param(
+    [parameter(mandatory=$true)][String]$url,
+    [parameter(mandatory=$true)][String]$library,
+    [parameter(mandatory=$true)][String]$username,
+    [parameter(mandatory=$true)][String]$password,
+    [parameter(mandatory=$true)][String]$path
+) 
+
+# CSOMモジュールの読み込み
+[System.Reflection.Assembly]::LoadWithPartialName("Microsoft.SharePoint.Client") > $null
+[System.Reflection.Assembly]::LoadWithPartialName("Microsoft.SharePoint.Client.Runtime") > $null
+
+
+# ログイン処理
+$securepassword = ConvertTo-SecureString $password -AsPlainText -Force
+$ctx = New-Object Microsoft.SharePoint.Client.ClientContext($url)
+$ctx.Credentials = New-Object Microsoft.SharePoint.Client.SharePointOnlineCredentials($username, $securepassword)
+
+# "For Each" loop will upload all of the files one by one onto the destination using the UploadFile method
+$filesCollectionInSourceDirectory=Get-ChildItem $path -File   
+
+ForEach ($oneFile in $filesCollectionInSourceDirectory) {
+      
+    $list = $ctx.Web.Lists.GetByTitle($library)
+    $ctx.Load($list)
+    $ctx.ExecuteQuery()     
+   
+    $SourceFilePath=$oneFile.FullName
+    $targetFilePath=$url+"/"+"$library"+"/"+$oneFile
+
+    $fileOpenStream = New-Object IO.FileStream($SourceFilePath, [System.IO.FileMode]::Open)  
+    $fileCreationInfo = New-Object Microsoft.SharePoint.Client.FileCreationInformation  
+    $fileCreationInfo.Overwrite = $true  
+    $fileCreationInfo.ContentStream = $fileOpenStream  
+    $fileCreationInfo.URL = $oneFile
+    $uploadFileInfo = $list.RootFolder.Files.Add($FileCreationInfo)  
+    $ctx.Load($uploadFileInfo)  
+    $ctx.ExecuteQuery() 
+     
+    Write-host -f Green "File '$SourceFilePath' has been uploaded to '$targetFilePath' successfully!"
+}
