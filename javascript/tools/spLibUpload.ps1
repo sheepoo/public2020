4cd4476a3a04ad2ce6af7ad62392b51cc8f5e87d
SHINOBU ISHIDA <SHINOBUI@jp.ibm.com>
2020-07-21 21:45:48 +0900

    CSOM -> PnP Powershell

--- javascript/tools/spLibUpload.ps1
+++ javascript/tools/spLibUpload.ps1
@@ -4,9 +4,6 @@
 実行形式：
 ./spLibUpload.ps1 -url [サイトのURL] -library [アップロード先ライブラリ名] -username [ユーザー名] -password [パスワード] -path [対象のファイルのあるディレクトリのフルパス]
 #>
-#
-# SharePoint Online Automation – O365 – Upload files to document library using PowerShell CSOM
-# https://global-sharepoint.com/powershell/upload-files-to-sharepoint-online-document-library-using-powershell-csom/
 
 
 # 引数取得(サイトURL、ユーザー名、パスワード、登録するファイルのあるフォルダ)
@@ -18,53 +15,24 @@ Param(
     [parameter(mandatory=$true)][String]$path
 ) 
 
-# CSOMライブラリの読み込み
-Add-Type -Path "/home/circleci/.local/share/powershell/Modules/SharePointOnline.CSOM/1.0.5/Microsoft.SharePoint.Client.dll"
-Add-Type -Path "/home/circleci/.local/share/powershell/Modules/SharePointOnline.CSOM/1.0.5/Microsoft.SharePoint.Client.Runtime.dll"
-
 # ログイン処理
-$securepassword = ConvertTo-SecureString $password -AsPlainText -Force
-$ctx = New-Object Microsoft.SharePoint.Client.ClientContext($url)
-$ctx.Credentials = New-Object Microsoft.SharePoint.Client.SharePointOnlineCredentials($username, $securepassword)
+$securePassword = convertto-securestring -String $password -AsPlainText -Force
+$cred = new-object -typename System.Management.Automation.PSCredential -argumentlist $username, $securePassword
+Connect-PnPOnline -Url $url -Credentials $cred
 
 # "For Each" loop will upload all of the files one by one onto the destination using the UploadFile method
 $filesCollectionInSourceDirectory=Get-ChildItem $path -File   
 
 ForEach ($oneFile in $filesCollectionInSourceDirectory) {
-
     try {   
-            #リスト一覧を取得
-            $ctx.Load($ctx.Web.lists)
-            $ctx.ExecuteQuery()
-            foreach($objList in $ctx.Web.lists)
-            {
-                $listTitle = $objList.Title
-                Write-Host "リスト ${listTitle} をエクスポートします"
-            }
-
-            Write-Host "ここからライブラリ処理"
-            $list = $ctx.Web.Lists.GetByTitle($library)
-            $ctx.Load($list)
-            $ctx.ExecuteQuery()     
-            Write-Host "ここからアップロード"
-
             $SourceFilePath=$oneFile.FullName
-            $targetFilePath=$url+"/"+"$library"+"/"+$oneFile.Name
-            
-            $fileOpenStream = New-Object IO.FileStream($SourceFilePath, [System.IO.FileMode]::Open)  
-            $fileCreationInfo = New-Object Microsoft.SharePoint.Client.FileCreationInformation  
-            $fileCreationInfo.Overwrite = $true  
-            $fileCreationInfo.ContentStream = $fileOpenStream  
-            $fileCreationInfo.URL = $oneFile
-            $uploadFileInfo = $list.RootFolder.Files.Add($FileCreationInfo)  
-            $ctx.Load($uploadFileInfo)  
-            $ctx.ExecuteQuery() 
-            
-            Write-host "File '$SourceFilePath' has been uploaded to '$targetFilePath' successfully!"
+            $pnplibrary = "/"+$library
+            $file = Add-PnPFile -Path $SourceFilePath -Folder "$pnplibrary" 
+            Write-host "File '$SourceFilePath' has been uploaded to '$pnplibrary' successfully!"
     }
     catch {
         Write-Error ($_.Exception)
-        Write-Host "Upload error : File '$SourceFilePath'"
+        Write-Host "Upload error : File $SourceFilePath"
         exit 1
     }
 }
